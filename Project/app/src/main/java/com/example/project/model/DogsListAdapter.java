12c9ac81bdda8d9f01698adb6197cc26b71d7faa
kuefmz <jenifer.girl.98@gmail.com>
2021-12-12 19:26:37 +0100

    merge 2 adapters

--- Project/app/src/main/java/com/example/project/model/DogsListAdapter.java
+++ Project/app/src/main/java/com/example/project/model/DogsListAdapter.java
@@ -1,10 +1,8 @@
 package com.example.project.model;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,30 +13,16 @@ import com.example.project.DogsDataActivity;
 import com.example.project.MainActivity;
 import com.example.project.R;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DogsListAdapter extends BaseAdapter {
     private List<Dogs> data = new ArrayList<>();
-    private Context main_ctx;
+    private Context ctx;
 
     public DogsListAdapter(List<Dogs> dogs, Context ctx) {
         data.addAll(dogs);
-        main_ctx = (MainActivity)ctx;
+        this.ctx = ctx;
     }
     public void addAll(List<Dogs> dogs) {
         data.clear();
@@ -64,35 +48,29 @@ public class DogsListAdapter extends BaseAdapter {
     public View getView(int i, View view, ViewGroup viewGroup) {
 
         LayoutInflater inflater;
-        inflater = (LayoutInflater) main_ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         if (view == null) {
             view = inflater.inflate(R.layout.dogs_row_layout , viewGroup,false );
         }
 
-        ImageView imageView = view.findViewById(R.id.image_dog);
-        //imageView.getLayoutParams().width = 100;
-        //imageView.setScaleType();
-        int newWidth = 350;
-        int newHeight = 315;
-
-        //Log.d("apple", Integer.toString(newWidth) + ", " + Integer.toString(newHeight));
+        final int newWidth = 350;
+        final int newHeight = 315;
 
         Bitmap newbitMap = Bitmap.createScaledBitmap(data.get(i).getBmpImage(), newWidth, newHeight, true);
 
-        //imageView.setImageBitmap(data.get(i).getBmpImage());
+        ImageView imageView = view.findViewById(R.id.image_dog);
         imageView.setImageBitmap(newbitMap);
-        imageView.setClickable(true);
-
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent iNext = new Intent(main_ctx, DogsDataActivity.class);
+                Intent iNext = new Intent(ctx, DogsDataActivity.class);
                 iNext.putExtra(MainActivity.PARAMETER_DOG_TYPE, data.get(i).getType());
                 iNext.putExtra(MainActivity.PARAMETER_DOG_IMAGE_URL, data.get(i).getImageUrl());
                 iNext.putExtra(MainActivity.PARAMETER_DOG_INDEX, Integer.toString(i));
+                iNext.putExtra(MainActivity.PARAMETER_FILENAME, data.get(i).getFilename());
 
-                main_ctx.startActivity(iNext);
+                ctx.startActivity(iNext);
             }
         });
 
